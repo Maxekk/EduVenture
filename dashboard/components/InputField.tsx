9c90a38d777fb87c85a6f5793cedf250ba663514
Maximilien Klima <maxeklima@gmail.com>
2023-06-04 11:31:55 +0200

    Login page structure finished

--- dashboard/components/InputField.tsx
+++ dashboard/components/InputField.tsx
@@ -2,13 +2,14 @@ import React from "react";
 
 type Props = {
   label: String;
+  isPassword: Boolean
 };
 
-function InputField({ label }: Props) {
+function InputField({ label, isPassword }: Props) {
   return (
-    <div className="flex flex-col mt-12 gap-2 w-[100%] items-center justify-center">
-      <label className="text-white text-2xl">{label}</label>
-      <input type="text" className="w-[60%] h-[60px] bg-[#484948]"></input>
+    <div className="flex flex-col mt-24 gap-2 w-[100%] items-center justify-center text-left">
+      <label className="text-white text-4xl text-left font-serif">{label}</label>
+      <input type={`${isPassword ? "password" : "email"}`} placeholder={`${isPassword ? "" : "somemail@gmail.com"}`} className="w-[70%] h-[80px] bg-[#484948] border-b-2 border-l-0 border-r-0 border-t-0 text-3xl text-white"></input>
     </div>
   );
 }
--- dashboard/components/LoginScreen.tsx
+++ dashboard/components/LoginScreen.tsx
@@ -12,10 +12,14 @@ function LoginScreen({}: Props) {
         <Image src={logo} width={400} height={200} alt="EduVenture logo" />
       </div>
       <div className="w-[100%] h-[80%] flex items-center justify-center">
-        <div className="w-[40%] h-[80%]">
-          <div className="w-[100%] h-[70%] flex flex-col gap-5 items-center justify-center">
-            <InputField label="Email" />
-            <InputField label="Password" />
+        <div className="w-[40%] h-[100%]">
+          <div className="w-[100%] h-[100%] flex flex-col gap-5 justify-center">
+            <div className="w-[100%] h-[100%] flex flex-col mt-0  items-center">
+            <InputField label="Email" isPassword={false}/>
+            <InputField label="Password" isPassword={true}/>
+
+            <button className="bg-transparent w-[250px] h-[70px] mt-24 text-white font-mono text-3xl border-4 border-green-500 transition-[1s] hover:bg-green-500">Login</button>
+            </div>
           </div>
         </div>
       </div>
