332cfc1096cf532d22983d26534ac01723a3b011
Maximilien Klima <maxeklima@gmail.com>
2023-06-22 16:50:57 +0200

    -displaying data (users count) in home component

--- dashboard/components/Home.tsx
+++ dashboard/components/Home.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Image from "next/image";
 import logo from "../public/logo.png";
 import {GiTeacher} from 'react-icons/gi'
@@ -7,6 +7,18 @@ import {MdSchool} from 'react-icons/md'
 type Props = {}
 
 function Home({}: Props) {
+  const [data,setdata] = useState({})
+  
+  const fetchUserData = async () => {
+    const req = await fetch(`http://localhost:8080/getUsrCount`);
+    const res = await req.json();
+    setdata(res)
+  };
+
+  useEffect(() => {
+    fetchUserData()
+  },[])
+
   return (
     <div className='w-[100%] h-[100%]'>
       <div className='w-[100%] h-[50%] flex justify-center items-center'>
@@ -16,7 +28,7 @@ function Home({}: Props) {
         <div className='bg-[#F2F4F8] w-[500px] h-[300px] rounded-3xl border-slate-300 border-2 mt-[-5vh] mr-12 shadow-md flex'>
           <div className='w-[60%] h-[100%] text-4xl p-4'>
             <div className='h-[50%] w-[100%] p-4'>Current count of students</div>
-            <div className='h-[50%] w-[100%] text-8xl align-text-bottom p-2 mt-4 ml-2'>0</div>
+            <div className='h-[50%] w-[100%] text-8xl align-text-bottom p-2 mt-4 ml-2'>{data.totalStudents}</div>
           </div>
           <div className='w-[40%] h-[100%] flex items-center justify-center text-gray-600'> 
           <MdSchool className='w-[200px] h-[200px] mr-6'/>
@@ -27,7 +39,7 @@ function Home({}: Props) {
       <div className='bg-[#F2F4F8] w-[500px] h-[300px] rounded-3xl border-slate-300 border-2 mt-[-5vh] mr-12 shadow-md flex'>
           <div className='w-[60%] h-[100%] text-4xl p-4'>
             <div className='h-[50%] w-[100%] p-4'>Current count of teachers</div>
-            <div className='h-[50%] w-[100%] text-8xl align-text-bottom p-2 mt-4 ml-2'>0</div>
+            <div className='h-[50%] w-[100%] text-8xl align-text-bottom p-2 mt-4 ml-2'>{data.totalTeachers}</div>
           </div>
           <div className='w-[40%] h-[100%] flex items-center justify-center text-gray-600'>
           <GiTeacher className='w-[200px] h-[200px] mr-6'/>
